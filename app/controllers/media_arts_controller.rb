81eca96fb25f615f10072d965263c5623dcefd71
samnangcattor <kcattor@gmail.com>
2016-09-20 11:23:15 +0700

    show each media art

--- app/controllers/media_arts_controller.rb
+++ app/controllers/media_arts_controller.rb
@@ -3,4 +3,9 @@ class MediaArtsController < ApplicationController
     @media_arts = MediaArt.all
     respond_to :html, :json
   end
+
+  def show
+    @media_art = MediaArt.find params[:id]
+    respond_to :html, :json
+  end
 end
---
+++ app/views/media_arts/index.json.jbuilder
@@ -0,0 +1,1 @@
+json.array! @media_arts, partial: "media_arts/media_art", as: :media_art
