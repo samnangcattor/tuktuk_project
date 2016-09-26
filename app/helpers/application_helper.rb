c7497e9c519f8e855de1a1baba68efe43e772361
samnangcattor <kcattor@gmail.com>
2016-09-26 10:21:08 +0700

    refactor code show brand

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def rediect_filter fitler_path, fitler_id
-    fitler_path = "media_art" if fitler_path == Settings.filters.media_art
-    path = fitler_path.downcase + "s"
+    fitler_path = "media_arts" if fitler_path == Settings.filters.media_art
+    path = fitler_path.downcase
     path + "/" + fitler_id
   end
 end
