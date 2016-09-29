693e5cc4c6359c33b07e5bea085ae022a0706f55
samnangcattor <kcattor@gmail.com>
2016-09-29 15:38:34 +0700

    zoom photo product when hover

--- spec/factories/media_arts.rb
+++ spec/factories/media_arts.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
   factory :media_art do
-    name {["media art 1", "media art 2", "media art 3", "media art 4"].sample}
+    name {["Media Art 1", "Media Art 2", "Media Art 3", "Media Art 4"].sample}
   end
 end
